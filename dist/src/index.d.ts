2588d479397a039f4cc6bb5b1cb342369ee38d3b
Will McKinley <willowmck@gmail.com>
2020-04-29 16:47:38 -0400

    Add gitignore and remove temporary folders

--- dist/src/index.d.ts
+++
@@ -1,4 +0,0 @@
-import "dotenv/config";
-import KcAdminClient from 'keycloak-admin';
-export declare const kcAdminClient: KcAdminClient;
-export declare const start: () => Promise<void>;
--- dist/src/routes/groups.js
+++
@@ -1,1 +0,0 @@
-//# sourceMappingURL=groups.js.map
